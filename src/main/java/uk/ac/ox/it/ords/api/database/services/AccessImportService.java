464641f5f2a11732254ecee5649fe37f9e918a14
ouit0465 <ouit0465@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-10-24 18:29:42 +0000

    Structural Additions

--- src/main/java/uk/ac/ox/it/ords/api/database/services/AccessImportService.java
+++ src/main/java/uk/ac/ox/it/ords/api/database/services/AccessImportService.java
@@ -1,5 +1,50 @@
+/*
+ * Copyright 2015 University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.ac.ox.it.ords.api.database.services;
+
+import java.io.File;
+import java.util.Map;
+
 
 public interface AccessImportService {
 
+	/**
+	 * Check that a schema can be successfully created for this database
+	 * @param database
+	 * @return true if we are confident that an import can be performed
+	 */
+	public abstract boolean preflightImport(File database);
+	
+	/**
+	 * Create a schema from the supplied Access database file
+	 * @param databaseServer
+	 * @param databaseName
+	 * @param database
+	 * @return a map of results for each table; true if the table was successfully created
+	 */
+	public abstract Map<String, TableImportResult> createSchema(String databaseServer, String databaseName, File database, String user);
+	
+	/**
+	 * Import data from supplied Access database file. This method must only be called after creating a schema.
+	 * @param databaseServer
+	 * @param databaseName
+	 * @param database
+	 * @return a map of results for each table; true if the data is successfully imported for that table
+	 */
+	public abstract Map<String, TableImportResult> importData(String databaseServer, String databaseName, File database, String user);
+	
 }
--- src/main/java/uk/ac/ox/it/ords/api/database/services/CSVService.java
+++ src/main/java/uk/ac/ox/it/ords/api/database/services/CSVService.java
@@ -1,5 +1,115 @@
+/*
+ * Copyright 2015 University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.ac.ox.it.ords.api.database.services;
 
+import java.io.File;
+
+
+import uk.ac.ox.it.ords.api.database.structure.TableData;
+
 public interface CSVService {
+	/**
+	 * Export a database table as a CSV file
+	 * @param database
+	 * @param tableData
+	 * @return the File written with the data from the table
+	 */
+	public abstract File exportTable(String server, String database, TableData tableData, String user, String password) throws Exception;
+	
+	/**
+	 * Export a database table as a CSV file
+	 * @param dbname 
+	 * @param tablename
+	 * @return the File written with the data from the table
+	 */
+	public abstract File exportTable(String server, String dbName, String tableName, String user, String password) throws Exception;
+	
+	/**
+	 * Export a database table as a CSV file
+	 * @param database
+	 * @param tableData
+	 * @param file the file to export the data to
+	 * @return the File written with the data from the table
+	 */
+	public abstract File exportTable(String server, String database, TableData tableData, File file, String user, String password) throws Exception;
+
+	/**
+	 * Export a pre-run database query as a CSV file
+	 * @param database
+	 * @param query the query
+	 * @return the File written with the data from the table
+	 */
+	public abstract File exportQuery(String server, String database, String query, String user, String password) throws Exception;
+	
+	/**
+	 * Export a database table as a CSV file
+	 * @param database
+	 * @param query the query
+	 * @param file the file to export the data to
+	 * @return the File written with the data from the table
+	 */
+	public abstract File exportQuery(String server, String database, String query, File file, String user, String password) throws Exception;
+
+	/**
+	 * Read data from a CSV file and create a new table in the database
+	 * @param database the name of the database to import into
+	 * @param newTableName the name to use for the resulting table
+	 * @param file the file to import
+	 * @param headerRow true if the first row of the file are the column headers
+	 * @return
+	 */
+	public abstract TableData newTableDataFromFile(String server, String database, String newTableName, File file, boolean headerRow, String user, String password) throws Exception;
+
+	
+	/**
+	 * Read data from a CSV file and create a new table in the database
+	 * @param database the name of the database to import into
+	 * @param file the file to import
+	 * @param headerRow true if the first row of the file are the column headers
+	 * @return
+	 */
+	public abstract TableData newTableDataFromFile(String server, String database, File file, boolean headerRow, String user, String password) throws Exception;
+	
+	/**
+	 * Read data from a CSV file and create a new table in the database
+	 * @param database the name of the database to import into
+	 * @param newTableName the name to use for the resulting table
+	 * @param file the file to import
+	 * @param headerRow true if the first row of the file are the column headers
+	 * @param addPrimaryKeyColumn when set to true the resulting table will also have a generated PK column
+	 * @return
+	 */
+	public abstract TableData newTableDataFromFile(String server, String database, String newTableName, File file, boolean headerRow, boolean addPrimaryKeyColumn, String user, String password) throws Exception;
+
+	/**
+	 * Read data from a CSV file and append it to an existing table in the database
+	 * @param database the name of the database to import into
+	 * @param tableName the name of the table to import the data into
+	 * @param file the file to read data from
+	 */
+	public abstract void appendDataFromFile(String server, String database, String tableName, File file, String user, String password)  throws Exception;
+	
+	/**
+	 * Read data from a CSV file and append it to an existing table in the database
+	 * @param database the name of the database to import into
+	 * @param tableName the name of the table to import the data into
+	 * @param file the file to read data from
+	 * @param headerRow true if the first row of data is to be interpreted as a header row
+	 */
+	public abstract void appendDataFromFile(String server, String database, String tableName, File file, boolean headerRow, String user, String password)  throws Exception;
 
 }
