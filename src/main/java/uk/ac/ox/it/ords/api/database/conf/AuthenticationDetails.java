5fcaffbd2ff68b0d06ea4e5d47cd73ca8ceef209
scottbw <scott.bradley.wilson@gmail.com>
2016-04-25 15:16:54 +0100

    Removed unused code

--- src/main/java/uk/ac/ox/it/ords/api/database/conf/AuthenticationDetails.java
+++ src/main/java/uk/ac/ox/it/ords/api/database/conf/AuthenticationDetails.java
@@ -1,11 +1,6 @@
 package uk.ac.ox.it.ords.api.database.conf;
 
-import java.io.IOException;
-import java.util.Properties;
-
 import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,109 +9,24 @@ import uk.ac.ox.it.ords.security.configuration.MetaConfiguration;
 
 
 public class AuthenticationDetails {
-	private static Logger log = LoggerFactory.getLogger(AuthenticationDetails.class);
-	private String ordsReadOnlyUser, ordsReadOnlyPassword;
+
 	private String ordsUser, ordsPassword;
-	private String datasetViewer, datasetViewerPassword;
-	private String rootDbUser, rootDbPassword;
-	private String ordsOdbcUserMasterPassword;
 	
-	protected static String ODBC_MASTER_PASSWORD_PROPERTY = "ords.odbc.masterpassword";
 	protected static String ORDS_DATABASE_NAME = "ords.database.name";
 	protected static String ORDS_DATABASE_USER = "ords.database.user";
 	protected static String ORDS_DATABASE_PASSWORD = "ords.database.password";
 	protected static String ORDS_DATABASE_HOST = "ords.database.server.host";
-	
-	protected static String ORDS_READONLY_USER = "ords.database.readonlyuser";
-	protected static String ORDS_READONLY_PASSWORD = "ords.database.readonlypassword";
-	protected static String ORDS_DATASET_VIEWER = "ords.database.datasetviewer";
-	protected static String ORDS_DATASET_PASSWORD = "ords.datasetviewerpassword";
-	
-	protected static String ORDS_DATABASE_ROOT_USER = "ords.database.rootdbuser";
-	protected static String ORDS_DATABASE_ROOT_PASSWORD = "ords.database.rootdbpassword";
-	
-	/*
-	 * ords.server.configuration=serverConfig.xml
-ords.database.readonlyuser=ordsreadonly
-ords.database.readonlypassword=
-ords.database.datasetviewer=datasetViewer
-ords.datasetviewerpassword=ords.database.name=ords2
-ords.database.username=ords
-ords.database.password=ords
-ords.database.server.host=localhost
-ords.database.rootdbuser=ords
-ords.database.rootdbpassword=ords
-ords.odbc.masterpassword=iU7*%fiXkls
-ords.database.name=ords2
-	 */
-	
-	
 
 	public AuthenticationDetails() {
-			//String dbPropertiesLocation = MetaConfiguration
-			//		.getConfigurationLocation("databaseProperties");
-			//PropertiesConfiguration prop = new PropertiesConfiguration(
-			//		dbPropertiesLocation);
-			
-			Configuration properties = MetaConfiguration.getConfiguration();
 
-			//Properties prop = GeneralWebServicesUtils.readProperties(CommonVars.mainPropertiesFile);
+			Configuration properties = MetaConfiguration.getConfiguration();
 			
 			/**
 			 * The user that can connect to and read/write from/to the ords database. Used for hibernate.
 			 */
 			ordsUser = properties.getString(AuthenticationDetails.ORDS_DATABASE_USER);
 			ordsPassword = properties.getString(AuthenticationDetails.ORDS_DATABASE_PASSWORD);
-			
-			/**
-			 * A user that can read from (but not write to) users databases. Used to gather statistics (number of records held by ords).
-			 * TODO
-			 * Currently thinking this is no longer needed - each user database is accessible via that user's ords ODBC role and there is
-			 * always an owner so this could be used to collect statistics.
-			 * Thus for now I shall deprecate this
-			 */
-			ordsReadOnlyUser = properties.getString(AuthenticationDetails.ORDS_READONLY_USER);
-			ordsReadOnlyPassword = properties.getString(AuthenticationDetails.ORDS_READONLY_PASSWORD);
-			
-			/**
-			 * A read only user used to view public datasets
-			 */
-			datasetViewer = properties.getString(AuthenticationDetails.ORDS_DATASET_VIEWER);
-			datasetViewerPassword = properties.getString(AuthenticationDetails.ORDS_DATASET_PASSWORD);
-			
-			/**
-			 * A root user on the server that can create databases and set the public schema permissions
-			 */
-			rootDbUser = properties.getString(AuthenticationDetails.ORDS_DATABASE_ROOT_USER);
-			rootDbPassword = properties.getString(AuthenticationDetails.ORDS_DATABASE_ROOT_PASSWORD);
-			
-			/**
-			 * When ords accesses data via a user command (e.g. the user logs in to a project and adds rows to a table in a 
-			 * database), ords will do that via a special user linked to that of the user logged in. E.g., fred logs in to
-			 * the project and performs the update: under the covers, ords will access the database with user fred_ords. 
-			 * ordsOdbcUserMasterPassword provides the password for this access.
-			 */
-			ordsOdbcUserMasterPassword = properties.getString(AuthenticationDetails.ODBC_MASTER_PASSWORD_PROPERTY);
-            if ((ordsOdbcUserMasterPassword == null) || (ordsOdbcUserMasterPassword.length() == 0) ) {
-                log.error("Unable to get odbc master password - defaulting");
-                ordsOdbcUserMasterPassword = ordsPassword;
-            }
-	}
 
-//	public String getOrdsReadOnlyUser() {
-//		return ordsReadOnlyUser;
-//	}
-//
-//	public String getOrdsReadOnlyPassword() {
-//		return ordsReadOnlyPassword;
-//	}
-
-	public String getDatasetViewer() {
-		return datasetViewer;
-	}
-
-	public String getDatasetViewerPassword() {
-		return datasetViewerPassword;
 	}
 
 	public String getOrdsUser() {
@@ -125,17 +35,5 @@ ords.database.name=ords2
 
 	public String getOrdsPassword() {
 		return ordsPassword;
-	}
-
-	public String getRootDbUser() {
-		return rootDbUser;
-	}
-	
-	public String getRootDbPassword() {
-		return rootDbPassword;
-	}
-	
-	public String getOrdsOdbcUserMasterPassword() {
-		return ordsOdbcUserMasterPassword;
 	}
 }
--- src/main/java/uk/ac/ox/it/ords/api/database/conf/CommonVars.java
+++ src/main/java/uk/ac/ox/it/ords/api/database/conf/CommonVars.java
@@ -1,75 +1,16 @@
 package uk.ac.ox.it.ords.api.database.conf;
 
 public class CommonVars {
-    public enum DataTypesSupported {
-        ACCESS, DB_DUMP, CSV
-    }
     
     /**
      * If useDifferentSchema is set to true, then a new schema is created to be used in a database
      * in place of the default public one. If false, then the database server's root user needs to be
      * available so that permissions can change on the public schema. 
      */
-    public static boolean useDifferentSchema = false;
     public static final String SCHEMA_NAME = "public";
-    
-	public static final boolean developmentPhase = true;
-	public static final String projectNameAppender = "ordsProject";
-
-
-	/**
-	 * This variable is useful for development. When set, the user does not need to log in to work with projects and
-	 * databases. Obviously this should be set to false when the site is live.
-	 */
-	public static final boolean disableLogins = false;
-    
-    
+        
     public static final String CONNECTABLE_DB = "postgres";//"template1"
 
-	/**
-	 * If this is set, users may have projects with the same name, else each project name must be
-	 * unique. All spaces are stripped from the project name upon test and tests are case insensitive, so "My project"
-	 * and "My Project" will be classed as the same name.
-	 */
-	public static boolean allowUserToHaveProjectsWithSameName = true;
-
-    // TODO this can't stay like this
-	public static final String ADDRESS_OF_IAM_WEBAPP = "http://129.67.241.37:8080/iam/ProjectRoleServlet";
-    
-    public static final String MessageServer = "129.67.241.43";
-    
-    
-    public static final String PROJECT_PAGE = "projects.jsp";
-    public static final String PREFS_PAGE = "preferences.jsp";
-    public static final String DATAVIEW_PAGE = "datasetView.jsp";
-    public static final String DATABASE_PAGE = "database.jsp";
-    public static final String HOME_PAGE = "index.jsp";
-    public static final String ERROR_PAGE = "unknownError.jsp";
-    public static final String PROJECT_NODE_TABLES_PAGE = "table.jsp";
-    public static final String PROJECT_NODE_QUERY_PAGE = "queryResult.jsp";
-    
-    public static final String configurationFolder = "/etc/ordsConfig/";
-    public static String serverConfig = configurationFolder + "serverConfig.xml";
-    public static String DB_PROPS = "./src/test/resources/props.properties";
-    
-    public static String hibernateFile = configurationFolder + "/hibernateSubset.cfg.xml";
-    public static String mainHibernateFile = configurationFolder + "/hibernate.cfg.xml";
-    public static String mainPropertiesFile = configurationFolder + "/db.properties";
-    
-    //public static String NULL_VALUE = "<null>"; - depricated
-    public static boolean USE_ODBC = true;
-    
-    
-    public static long ALLOWABLE_AGE_OF_ADDRESS_FOR_ODBC_ACCESS = 30*24*60*60*1000;//One month
-    
-    
     public static String BAD_CHARACTER_REPLACEMENT = "_";
-    
-    
-    // Trial projects
-    public static final int MAX_RECORDS_PER_TABLE_IN_FINAL_PROJECT = 100;
-    public static final int MAX_DATA_SPACE_FOR_TRIAL_MB = 200;
-    
-    // Normal projects
-    public static final int MAX_DATA_SPACE_MB = 10000;
+
 }
