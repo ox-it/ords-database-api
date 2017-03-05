53cf1bb464604278c6f850330ceb6d594f290253
krispopat <kjpopat@gmail.com>
2017-03-05 15:15:47 +0000

    Added uploadSizeException

---
+++ src/main/java/uk/ac/ox/it/ords/api/database/exceptions/UploadSizeException.java
@@ -0,0 +1,13 @@
+package uk.ac.ox.it.ords.api.database.exceptions;
+
+public class UploadSizeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UploadSizeException(String message) {
+		super(message);
+	}
+}
